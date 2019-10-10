1c12e523d22d254420b7109de201c0e1e6e0a448
Jonathan Cheng <jchen337@wch136-11.cs.ucr.edu>
2019-10-10 10:40:45 -0700

    lab 4

--- Lab4_SM/source/main.c
+++ Lab4_SM/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: jchen337
  *  Partner(s) Name: Matthew Fernandes
  *	Lab Section:
- *	Assignment: Lab #4  Exercise #2
+ *	Assignment: Lab #4  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,74 +12,99 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {Initial, Wait, Inc, Dec, Reset} next_state;
+enum Door_States {Initial, Wait, PPress, PRelease, YPress, YRelease, LockPress, LockRelease} next_state;
 
-void Tick() { 
+void Door_Tick() { 
 	switch (next_state) { //Transitions
 		case Initial:
 			next_state = Initial;
 			break;
-		case Reset:
-		case Inc:
-		case Dec:
 		case Wait:
-			if (PINA == 0x01) {
-				next_state = Inc;
+			if (PINA == 0x04) {
+				next_state = PPress;
 				break;
 			}
-			else if (PINA == 0x02) {
-				next_state = Dec;
-				break;
-			}
-			else if (PINA == 0x03) {
-				next_state = Reset;
+			else if (PINA = 0x80) {
+				next_state = LockPress;
 				break;
 			}
 			else {
 				next_state = Wait;
 				break;
 			}
-			
+		case PPress:
+			if (PINA == 0x04) {
+				next_state = PPress;
+				break;
+			}
+			else if (PINA == 0x00) {
+				next_state = PRelease;
+				break;
+			}
+			else {
+				next_state = Wait;
+				break;
+			}
+		case PRelease:
+			if (PINA == 0x02) {
+				next_state = YPress;
+				break;
+			}
+			else if (PINA == 0x00) {
+				next_state = PRelease;
+				break;
+			}
+			else {
+				next_state = Wait;
+				break;
+			}
+		case YPress:
+		case YRelease:
+		case LockPress:
+			if (PINA == 0x80) {
+				next_state = LockPress;
+				break;
+			}
+			else {
+				next_state = LockRelease;
+				break;
+			}
+		case LockRelease:
+			next_state = Wait;
+			break;			
 	}
 	switch (next_state) { //Actions
 		case Initial:
-			PORTC = 0x07;
+			PORTB = 0x01;
 			next_state = Wait;
 			break;
 		case Wait:
 			next_state = Wait;
 			break;
-		case Inc:
-			if (PORTC >= 9) {
-				PORTC = 0x09;
-				next_state = Wait;
-				break;
-			}
-			else {
-				PORTC = PORTC + 0x01;
-				next_state = Wait;
-				break;
-			}
-		case Dec:
-			if (PORTC <= 0) {
-				PORTC = 0x00;
-				next_state = Wait;
-				break;
-			}
-			else {
-				PORTC = PORTC - 0x01;
-				next_state = Wait;
-				break;
-			}
-		case Reset:
-			PORTC = 0x00;
-			next_state = Wait;
+		case PPress:
+			next_state = PPress;
+			break;
+		case PRelease:
+			next_state = PRelease;
+			break;
+		case YPress:
+		case YRelease:
+		case LockPress:
+			PORTB = 0x00;
+			next_state = LockPress;
+			break;
+		case LockRelease:
+			PORTB = 0x00;
+			next_state = LockRelease;
+			break;
+
 	}
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	next_state = Initial;
 
