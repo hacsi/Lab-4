b654e27ab9bf9e7111bb3ed8d8452fd985aa8f11
Jonathan Cheng <jchen337@wch136-11.cs.ucr.edu>
2019-10-11 12:24:02 -0700

    lab 4

--- Lab4_SM/source/main.c
+++ Lab4_SM/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: jchen337
  *  Partner(s) Name: Matthew Fernandes
  *	Lab Section: 023
- *	Assignment: Lab #4  Exercise #4
+ *	Assignment: Lab #4  Exercise #5
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,125 +12,23 @@
 #include "simAVRHeader.h"
 #endif
 
-enum Door_States {Initial, Wait, PPress, PRelease, YPress, YRelease, LockPress, LockRelease} next_state;
-
-void Door_Tick() { 
-	switch (next_state) { //Transitions
-		case Initial:
-			next_state = Initial;
-			break;
-		case Wait:
-			if (PINA == 0x04) {
-				next_state = PPress;
-				break;
-			}
-			else if (PINA == 0x80) {
-				next_state = LockPress;
-				break;
-			}
-			else {
-				next_state = Wait;
-				break;
-			}
-		case PPress:
-			if (PINA == 0x04) {
-				next_state = PPress;
-				break;
-			}
-			else if (PINA == 0x00) {
-				next_state = PRelease;
-				break;
-			}
-			else {
-				next_state = Wait;
-				break;
-			}
-		case PRelease:
-			if (PINA == 0x02) {
-				next_state = YPress;
-				break;
-			}
-			else if (PINA == 0x00) {
-				next_state = PRelease;
-				break;
-			}
-			else {
-				next_state = Wait;
-				break;
-			}
-		case YPress:
-			if (PINA == 0x02) {
-				next_state = YPress;
-				break;
-			}
-			else if (PINA == 0x00) {
-				next_state = YRelease;
-				break;
-			}
-			else {
-				next_state = Wait;
-				break;
-			}
-		case YRelease:
-			next_state = YRelease;
-			break;
-		case LockPress:
-			if (PINA == 0x80) {
-				next_state = LockPress;
-				break;
-			}
-			else {
-				next_state = LockRelease;
-				break;
-			}
-		case LockRelease:
-			next_state = Wait;
-			break;			
-	}
-	switch (next_state) { //Actions
-		case Initial:
-			PORTB = 0x01;
-			next_state = Wait;
-			break;
-		case Wait:
-			next_state = Wait;
-			break;
-		case PPress:
-			next_state = PPress;
-			break;
-		case PRelease:
-			next_state = PRelease;
-			break;
-		case YPress:
-			next_state = YPress;
-			break;
-		case YRelease:
-			PORTB = (!PORTB & 0x03);
-			next_state = Wait;
-			break;
-		case LockPress:
-			PORTB = 0x00;
-			next_state = LockPress;
-			break;
-		case LockRelease:
-			PORTB = 0x00;
-			next_state = LockRelease;
-			break;
-
-	}
-}
-
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-	next_state = Initial;
+
+	int comb[4];
+	unsigned char i = 0;
+
+	
 
     /* Insert your solution below */
     while (1) {
-	Door_Tick();
-	PORTC = next_state;
+	scanf("%c", comb)
+	scanf("%c", comb)
+	scanf("%c", comb)
+	scanf("%c", comb)
     }
     return 1;
 }
